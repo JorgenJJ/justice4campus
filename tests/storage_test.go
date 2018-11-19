5a169e5c7540e0a1d8aeee43b6802b40e79aa104
Christian:UX430@LTS18.04 <christian@skjerning.eu>
2018-11-19 11:57:44 +0100

    Heroku test

---
+++ tests/storage_test.go
@@ -0,0 +1,15 @@
+package sotrage
+
+import (
+	//"github.com/JorgenJJ/justice4campus/internal/storage"
+	"fmt"
+	"testing"
+)
+
+func MainTest(m *testing.M) {
+	fmt.Println("Testing")
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
